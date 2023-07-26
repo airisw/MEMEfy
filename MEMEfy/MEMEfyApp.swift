6f08ca8cdcd8a4535be31462f0aea59da5d96b0b
Airis <airiswang@hotmail.com>
2023-07-26 00:49:00 -0400

    Initial Commit

---
+++ MEMEfy/MEMEfyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEMEfyApp.swift
+//  MEMEfy
+//
+//  Created by Airis Wang on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct MEMEfyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
