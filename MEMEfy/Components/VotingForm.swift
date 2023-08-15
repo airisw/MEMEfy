a5d7dbf12bb619073cd0ef882062626e7dd432db
Airis <airiswang@hotmail.com>
2023-08-14 20:18:26 -0400

    fixed spacing

--- MEMEfy/Components/VotingForm.swift
+++ MEMEfy/Components/VotingForm.swift
@@ -18,7 +18,7 @@ struct VotingForm: View {
         ScrollView {
             Button {
                 firebaseManager.fetchWinnerId(roomCode: firebaseManager.finalRoomCode, winnerGIF: selectedGif) {
-                        firebaseManager.updateWinner(roomCode: firebaseManager.finalRoomCode)
+                    firebaseManager.updateWinner(roomCode: firebaseManager.finalRoomCode)
                     
                     firebaseManager.scorePoint(roomCode: firebaseManager.finalRoomCode)
                 }
